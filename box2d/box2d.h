d8e22447c2bd47efd81fe705d057faec43dc8513
Alex Kremer <godexsoft@gmail.com>
2012-07-02 20:26:52 +0100

    box2d added

---
+++ box2d/box2d.h
@@ -0,0 +1,13 @@
+//
+//  box2d.h
+//  box2d
+//
+//  Created by Alex Kremer on 6/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface box2d : NSObject
+
+@end
---
+++ box2d/box2d.m
@@ -0,0 +1,13 @@
+//
+//  box2d.m
+//  box2d
+//
+//  Created by Alex Kremer on 6/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "box2d.h"
+
+@implementation box2d
+
+@end
