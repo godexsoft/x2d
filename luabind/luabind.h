4f46715866a101010b803eaac3be7945ebd4b55e
Alex Kremer <godexsoft@gmail.com>
2012-08-22 02:23:17 +0100

    luabind and lua compile

---
+++ luabind/luabind.h
@@ -0,0 +1,13 @@
+//
+//  luabind.h
+//  luabind
+//
+//  Created by Alex Kremer on 8/22/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface luabind : NSObject
+
+@end
---
+++ luabind/luabind.m
@@ -0,0 +1,13 @@
+//
+//  luabind.m
+//  luabind
+//
+//  Created by Alex Kremer on 8/22/12.
+//
+//
+
+#import "luabind.h"
+
+@implementation luabind
+
+@end
